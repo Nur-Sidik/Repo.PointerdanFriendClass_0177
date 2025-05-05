85ffc8be9af5066b36af8992be7a9f712d2799f1
Nur-Sidik <nursidik1500@gmail.com>
2025-05-05 16:21:30 +0700

    membuat pointer deferences

---
+++ PointerDua.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa
+{
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main(){
+    Mahasiswa *mhs = new Mahasiswa(1);
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
