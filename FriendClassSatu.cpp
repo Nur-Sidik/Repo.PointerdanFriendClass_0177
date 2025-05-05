cf335fb3d53e27f1ece08dc7d307af46e2067838
Nur-Sidik <nursidik1500@gmail.com>
2025-05-05 16:20:01 +0700

    membuat friend function

---
+++ FriendClassSatu.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayALL(orang &a);
+};
+
+void siswa::displayALL(orang &a){
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pnama){
+    nama = pnama;
+}
+void siswa::setId(int pId){
+    id = pId;
+}
+
+int main (){
+    orang joko;
+    joko.setNama("Joko Kumat");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayALL(joko);
+    return 0;
+}
